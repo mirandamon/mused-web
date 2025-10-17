e33e5b2a622dc07ca3e9aba52bd0b36bd8faaa00
Nathan Miranda <nate845@gmail.com>
2025-10-17 00:21:58 -0400

    Adding react-native-config for .env variables

--- mused-rn/app/lib/firebase.ts
+++ mused-rn/app/lib/firebase.ts
@@ -1,17 +1,22 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {
+  getAuth,
+  getReactNativePersistence,
+  initializeAuth,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import Config from "react-native-config";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: Config.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: Config.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: Config.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: Config.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: Config.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
